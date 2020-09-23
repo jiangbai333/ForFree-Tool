b9ae2acaed2373866ed7554476dcdb75bd1362c3
姜柏超 <jiangbai333@gmail.com>
2020-09-23 20:58:00 +0800

    新增了forfree-hex子项目

--- forfree-core/src/test/java/idni/jbc/forfree/AppTest.java
+++ forfree-core/src/test/java/idni/jbc/forfree/AppTest.java
@@ -1,25 +1,38 @@
 package idni.jbc.forfree;
 
-import idni.jbc.forfree.App;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest {
-
-    private App app;
-
-    @Before
-    public void init() {
-        app = new App();
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
     }
 
-    @Test
-    public void testAdd() {
-        Assert.assertEquals(2, app.add(1,1));
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
     }
 }
