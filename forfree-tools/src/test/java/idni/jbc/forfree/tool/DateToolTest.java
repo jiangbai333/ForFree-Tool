5e74b41a51d879b0fccacdfdc31a25d15c5326fa
姜柏超 <jiangbai333@gmail.com>
2020-11-03 14:02:25 +0800

    新建测试分支，提交测试

--- forfree-tools/src/test/java/idni/jbc/forfree/tool/DateToolTest.java
+++ forfree-tools/src/test/java/idni/jbc/forfree/tool/DateToolTest.java
@@ -16,9 +16,9 @@ public class DateToolTest {
     @Test
     public void testMonth() {
 
-        Assert.assertEquals(10, DateTool.month());
-        Assert.assertEquals(10, DateTool.month(new Date()));
-        Assert.assertEquals(10, DateTool.month(Calendar.getInstance()));
+        Assert.assertEquals(11, DateTool.month());
+        Assert.assertEquals(11, DateTool.month(new Date()));
+        Assert.assertEquals(11, DateTool.month(Calendar.getInstance()));
     }
 
     @Test
