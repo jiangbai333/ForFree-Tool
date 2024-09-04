4fa86874c0a92453501a96179ae9ae1c720d0c1b
姜柏超 <jiangbai333@gmail.com>
2024-09-04 21:03:06 +0800

    🗯  chore: 图片压缩类的一些修改
    Signed-off-by: 姜柏超 <jiangbai333@gmail.com>

--- forfree-tools/src/main/java/idni/jbc/forfree/tool/image/Compress.java
+++ forfree-tools/src/main/java/idni/jbc/forfree/tool/image/Compress.java
@@ -1,23 +1,26 @@
 package idni.jbc.forfree.tool.image;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.util.Iterator;
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
 import javax.imageio.stream.ImageOutputStream;
-import java.awt.image.BufferedImage;
+
 import java.io.*;
-import java.util.Iterator;
 
 public class Compress {
     public static void main(String[] args) throws IOException, FileNotFoundException {
         System.out.println(args[0]);
         System.out.println(args[1]);
 
-        File input = new File("/Users/jiangbaichao/Desktop/img_48e967d7079331abcce319ca8661be00.png");
+        File input = new File("/Users/jiangbaichao/Desktop/head.png");
         BufferedImage image = ImageIO.read(input);
+        image = removeAlphaChannel(image);
 
-        File compressedImageFile = new File("/Users/jiangbaichao/Desktop/compressed_image.jpg");
+        File compressedImageFile = new File("/Users/jiangbaichao/Desktop/head_compressed.jpg");
         OutputStream os = new FileOutputStream(compressedImageFile);
 
         Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
@@ -29,11 +32,28 @@ public class Compress {
         ImageWriteParam param = writer.getDefaultWriteParam();
 
         param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-        param.setCompressionQuality(0.05f);  // Change the quality value you prefer
+        param.setCompressionQuality(0.15f);  // Change the quality value you prefer
         writer.write(null, new IIOImage(image, null, null), param);
 
         os.close();
         ios.close();
         writer.dispose();
+    }
+    private static BufferedImage removeAlphaChannel(BufferedImage img) {
+        if (!img.getColorModel().hasAlpha()) {
+            return img;
+        }
+
+        BufferedImage target = createImage(img.getWidth(), img.getHeight(), false);
+        Graphics2D g = target.createGraphics();
+        // g.setColor(new Color(color, false));
+        g.fillRect(0, 0, img.getWidth(), img.getHeight());
+        g.drawImage(img, 0, 0, null);
+        g.dispose();
+
+        return target;
+    }
+    private static BufferedImage createImage(int width, int height, boolean hasAlpha) {
+        return new BufferedImage(width, height, hasAlpha ? BufferedImage.TYPE_INT_ARGB : BufferedImage.TYPE_INT_RGB);
     }
 }
